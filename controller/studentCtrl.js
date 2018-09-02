aec16f928791eee21a84c2a57bf9b6df6adff095
Trường giang <giangkoolpark@gmail.com>
2018-09-03 00:01:09 +0700

    fix error

--- controller/studentCtrl.js
+++ controller/studentCtrl.js
@@ -17,12 +17,11 @@ module.exports = {
                     let _role = req.body.Role;
                     let _user = req.body.Username;
                     var _list_student = [];
-
                     if (_role[0].id === 0) {
                         response = { 'error_code': 0, 'student': data };
                     } else {
                         data.forEach(student => {
-                            if (_user === student.Manager[0].Username) {
+                            if (_user === student.Manager[0].id) {
                                 _list_student.push(student);
                             }
                         });
