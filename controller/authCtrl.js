799d457eb1ed76063c48da50ea7ed9e964361226
Trường giang <giangkoolpark@gmail.com>
2018-09-11 10:20:52 +0700

    ok

--- controller/authCtrl.js
+++ controller/authCtrl.js
@@ -259,6 +259,17 @@ module.exports = {
                 response = { 'error_code': 1, 'message': 'error fetching data' };
                 res.status(200).json(response);
             } else {
+                if (data !== null) {
+                    data.Leader = false;
+                    data.save(function (err) {
+                        if (err) {
+                            response = { 'error_code': 1, 'message': 'error fetching data' }
+                        } else {
+                            response = { 'error_code': 0, 'message': 'Update info success' }
+                        }
+                        res.status(200).json(response);
+                    })
+                }
                 data.Leader = false;
                 data.save(function (err) {
                     if (err) {
